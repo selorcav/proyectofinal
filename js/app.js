bdee0508f6995b4f33af718790cf797160570dc3
Sebastian <se.lorcav@gmail.com>
2019-01-16 21:33:43 -0300

    agregando js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready($(function(){
+	 $(".local__item--local1").on("click", function(){
+	 	$("#mondobirras").toggleClass("hidden");
+	 })
+	 $(".local__item--local2").on("click", function(){
+	 	$("#insertcoin").toggleClass("hidden");
+	 })
+	 $(".local__item--local3").on("click", function(){
+	 	$("#dublin").toggleClass("hidden");
+	 })
+	 $(".local__item--local4").on("click", function(){
+	 	$("#tquila").toggleClass("hidden");
+	 })
+	 $(".local__item--local5").on("click", function(){
+	 	$("#greenbull").toggleClass("hidden");
+	 })
+	 $(".local__item--local6").on("click", function(){
+	 	$("#laspalmas").toggleClass("hidden");
+	 })
+
+})
+)
